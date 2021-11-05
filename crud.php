f0fad697c869655a53747f6e0566417794ec3f24
Ana150 <anaoliveira4267@gmail.com>
2021-11-05 11:00:16 -0300

    Atualização 05/11

--- crud.php
+++ crud.php
@@ -10,7 +10,18 @@ function read($conn){
         $dados = mysqli_fetch_all($resultado);
         echo json_encode(array('status'=>'success', 'data'=>$dados));
     }else{
-        echo json_encode(array('status'=>'success', 'data'=>mysqli_error($conn)));
+        echo json_encode(array('status'=>'error', 'data'=>mysqli_error($conn)));
+    }
+}
+
+function readId($cod_pessoa, $conn){
+    $sql = "SELECT * FROM tbl_pessoa WHERE cod_pessoa = $cod_pessoa";
+
+    if ($resultado = mysqli_query($conn, $sql)) {
+        $dados = mysqli_fetch_all($resultado);
+        echo json_encode(array('status'=>'success', 'data'=>$dados));
+    }else{
+        echo json_encode(array('status'=>'error', 'data'=>mysqli_error($conn)));
     }
 }
 
@@ -21,7 +32,33 @@ function create($nome, $sobrenome, $email, $celular, $fotografia, $conn){
     if (mysqli_query($conn, $sql)) {
         echo json_encode(array('status'=>'success', 'data'=>'Dados inseridos com sucesso'));
     }else{
-        echo json_encode(array('status'=>'error', 'data'=>'Erro ao in serir dados'));
+        echo json_encode(array('status'=>'error', 'data'=>'Erro ao inserir dados'));
+    }
+}
+
+function update($cod_pessoa, $nome, $sobrenome, $email, $celular, $fotografia, $conn){
+    $sql = "UPDATE tbl_pessoa SET 
+    nome = '$nome',
+    sobrenome = '$sobrenome',
+    email = '$email', 
+    celular = '$celular', 
+    fotografia = '$fotografia'
+    WHERE cod_pessoa = $cod_pessoa";
+
+    if (mysqli_query($conn, $sql)) {
+        echo json_encode(array('status'=>'success', 'data'=>'Dados alterados com sucesso'));
+    }else{
+        echo json_encode(array('status'=>'error',  'data'=>mysqli_error($conn)));
+    }
+}
+
+function delete($cod_pessoa, $conn){
+    $sql = "DELETE FROM tbl_pessoa WHERE cod_pessoa = $cod_pessoa";
+
+    if (mysqli_query($conn, $sql)) {
+        echo json_encode(array('status'=>'success', 'data'=>'Dados excluidos com sucesso'));
+    }else{
+        echo json_encode(array('status'=>'error',  'data'=>mysqli_error($conn)));
     }
 }
 
--- index.php
+++ index.php
@@ -13,6 +13,11 @@ if ($acao == "read") {
     read($conn);
 }
 
+if ($acao == "readId") {
+    $cod_pessoa = $_REQUEST["cod_pessoa"];
+    readId($cod_pessoa, $conn);
+}
+
 if ($acao == "create") {
     $nome = $_REQUEST['nome'];
     $sobrenome = $_REQUEST['sobrenome'];
@@ -21,5 +26,22 @@ if ($acao == "create") {
     $fotografia = $_REQUEST['fotografia'];
 
     create($nome, $sobrenome, $email, $celular, $fotografia, $conn);
+}
+
+if ($acao == "update") {
+    $cod_pessoa = $_REQUEST['cod_pessoa'];
+    $nome = $_REQUEST['nome'];
+    $sobrenome = $_REQUEST['sobrenome'];
+    $email = $_REQUEST['email'];
+    $celular = $_REQUEST['celular'];
+    $fotografia = $_REQUEST['fotografia'];
+
+    update($cod_pessoa, $nome, $sobrenome, $email, $celular, $fotografia, $conn);
+}
+
+if ($acao == "delete") {
+    $cod_pessoa = $_REQUEST['cod_pessoa'];
+
+    delete($cod_pessoa, $conn);
 }
 ?>
